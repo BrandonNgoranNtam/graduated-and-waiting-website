f649add06d1705111c4e515dbe9e199b6550514e
brandonzoom886 <brandonzoom886@gmail.com>
2024-10-09 07:17:30 +0000

    Add vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { router } from "./Router";
+import { Analytics } from "@vercel/analytics/react"
+
 
 export default function App() {
     return (
         <ThemeProvider>
             <RouterProvider router={router} />
+            <Analytics/>
         </ThemeProvider>
     )
 }
